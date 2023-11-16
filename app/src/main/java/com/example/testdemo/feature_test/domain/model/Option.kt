f9c151089c1a5762ab0676644b76dbd50bac3efe
Bhavya Mehta <bhavyahmehta@outlook.com>
2023-11-16 18:18:38 +0530

    upgrade plugin version and solve crash by making rememberSavable datatype as parcelable.

--- app/src/main/java/com/example/testdemo/feature_test/domain/model/Option.kt
+++ app/src/main/java/com/example/testdemo/feature_test/domain/model/Option.kt
@@ -1,6 +1,10 @@
 package com.example.testdemo.feature_test.domain.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Option(
     val id: String,
     val option: String,
-)
+):Parcelable
